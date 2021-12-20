4c7e930927270307049c2c701bcd43007467b16d
Victor Lascares <vmlascares@gmail.com>
2021-12-19 21:18:13 -0600

    Instalando dependencias y conectando app a mongo atlas

--- api/index.js
+++ api/index.js
@@ -1,3 +1,16 @@
-module.exports = (req, res) => {
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+app.use(bodyParser.json())  // Metodo para agregar plugins a nuestro servidor
+app.use(cors())
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.get('*', (req, res) => {
     res.send('Hola Mundo')
-}
+})
+
+module.exports = app
