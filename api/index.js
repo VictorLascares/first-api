4df64c4dbd69ce1bc9ac40595747e80e7479c7dd
Victor Lascares <vmlascares@gmail.com>
2021-12-19 21:55:06 -0600

    Estructurando la app con rutas

--- api/index.js
+++ api/index.js
@@ -3,14 +3,15 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+const meals = require('./routes/meals')
+const orders = require('./routes/orders')
 const app = express()
 app.use(bodyParser.json())  // Metodo para agregar plugins a nuestro servidor
 app.use(cors())
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-app.get('*', (req, res) => {
-    res.send('Hola Mundo')
-})
+app.use('/api/meals', meals)
+app.use('/api/orders', orders)
 
 module.exports = app
